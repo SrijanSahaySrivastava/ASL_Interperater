54683b7a155c47bce1aaf317686f36a4ff2b5b35
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2024-02-24 20:38:57 +0530

    detections in seperate files

--- video_feed.py
+++ video_feed.py
@@ -4,32 +4,7 @@ import numpy as np
 
 import mediapipe as mp
 
-mp_holistic = mp.solutions.holistic
-mp_drawing = mp.solutions.drawing_utils
-
-
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image.flags.writeable = False
-    results = model.process(image)
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    return image, results
-
-
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(
-        image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION
-    )
-    mp_drawing.draw_landmarks(
-        image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS
-    )
-    mp_drawing.draw_landmarks(
-        image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS
-    )
-    mp_drawing.draw_landmarks(
-        image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS
-    )
+from Mediapipe_detections import mediapipe_detection, draw_landmarks, mp_holistic
 
 
 # Create a VideoCapture object
