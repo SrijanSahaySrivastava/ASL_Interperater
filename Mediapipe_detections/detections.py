b1128641c6b39456c8ca7cb6fb37dfd42651bf8e
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2024-02-24 20:50:26 +0530

    gitignore

--- Mediapipe_detections/detections.py
+++ Mediapipe_detections/detections.py
@@ -38,3 +38,4 @@ def extract_keypoints(results):
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21 * 3)
     return np.concatenate([pose, face, lh, rh])
+
